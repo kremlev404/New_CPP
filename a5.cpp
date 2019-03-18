769618529ed7af74fbd402fed30518e4bf835831
Anton <45079487+kremlev404@users.noreply.github.com>
2019-03-18 17:13:15 +0300

    Update a5.cpp

--- a5.cpp
+++ a5.cpp
@@ -1,8 +1,8 @@
 // Подключаем нужные библиотеки
 #include <iostream>
 #include <fstream>
-// Подключаем нашу локальную билиотеку
-#include "mylab.h"
+#include <cstring>
+#include <cstdio>
 #define u_int unsigned int
 // Подключаем пространство имен
 using std::cout;
@@ -10,62 +10,112 @@ using std::cin;
 using std::endl;
 using std::ifstream;
 using std::ofstream;
-using std::ios;
-//Используем отдельную переменную для сохранения символа конца строки
-char nl('\n');
+
 // Наводим красоту
 void logo(){
-	cout << "\033[37;1m\033[44m"<< "\n **********************************************************";
-	cout << "\033[37;1m\033[47m"<< "\n * Nizhniy Novgorod Technical University                  *";
-	cout << "\033[37;1m\033[47m"<< "\n * Study work number 3. Task number 12.                   *";
-	cout << "\033[37;1m\033[47m"<< "\n * Performed student 18-IVT-2 Kremlev Anton               *";
-	cout << "\033[37;1m\033[44m"<< "\n **********************************************************";
-	cout << "\x1b[39;49m" << endl << endl;;
+    cout << "\033[37;1m\033[44m"<< "\n **********************************************************";
+    cout << "\033[37;1m\033[47m"<< "\n * Nizhniy Novgorod Technical University                  *";
+    cout << "\033[37;1m\033[47m"<< "\n * Study work number 3. Task number 12.                   *";
+    cout << "\033[37;1m\033[47m"<< "\n * Performed student 18-IVT-2 Kremlev Anton               *";
+    cout << "\033[37;1m\033[44m"<< "\n **********************************************************";
+    cout << "\x1b[39;49m" << endl << endl;;
 }
 //Создаем функцию с подсказками пользователю
 void help()
 {
 	cout<< "You can choose 2 startup options: "<<endl;
-	cout<< "-с launch of the program in the mode of creating electronic tables"<<endl;
-	cout<< "-r launch of the program in the content reading mode text file"<<endl;
-	cout<< "Also you must specify the number [N] of entries and the [file_name]"<<endl;
-	cout<< "Final view of the program launch structure -с [N] [file_name] or -r [N] [file_name]"<<endl;
+	cout<< "-all to see all the information about the objects "<<endl;
+	cout<< "-one [object] to see the information about the choosen object"<<endl;
+	cout<< "Variants of objects:"<<endl;
+	cout<< "-Plane"<<endl;
+	cout<< "-Meteozond"<<endl;
+	cout<< "-Helicopter"<<endl;
+	cout<< "-Flying_robot"<<endl;
 }
 class Aircraft{
+protected:
+    std::string type_of_aircraft;
+    bool manual_contlor;
 public:
-	String type_of_aircraft;
-	bool manual_contlor;
+	void getInfo(){		
+		cout<<"You've choosen : "<<type_of_aircraft<<endl;
+        std::cout << std::boolalpha;
+        if (manual_contlor == true)
+        	cout<<"This type of Aircraft support manual control\n";
+        else 
+        	cout<<"This type of Aircraft doesn't support manual control\n";
+
+    }
 };
 class Piloted_aircraft : public Aircraft {
-	Piloted_aircraft():manual_contlor(true){}
+protected:
+    Piloted_aircraft(){
+        manual_contlor = true;
+    }
 };
 class Not_piloted_aircraft : public Aircraft{
-	Not_piloted_aircraft():manual_contlor(false){}
+protected:
+    Not_piloted_aircraft(){
+        manual_contlor = false;
+    }
 };
+
 class Plane : public Piloted_aircraft{
-	Plane():type_of_aircraft("Plane"){}
+public:
+    Plane() : Piloted_aircraft(){
+        type_of_aircraft ="Plane";
+    }
 };
 class Helicopter : public Piloted_aircraft{
-	Helicopter():type_of_aircraft("Helicopter"){}
+public:
+    Helicopter() : Piloted_aircraft(){
+        type_of_aircraft = "Helicopter";
+    }
 };
 class Meteozond : public Not_piloted_aircraft{
-	Meteozond():type_of_aircraft("Meteozond"){}
+public:
+    Meteozond() : Not_piloted_aircraft(){
+        type_of_aircraft = "Meteozond";
+    }
 };
 class Flying_robot : public Not_piloted_aircraft{
-	Flying_robot():type_of_aircraft("Flying_robot"){}
+public:
+    Flying_robot() : Not_piloted_aircraft(){
+        type_of_aircraft = "Flying_robot";
+    }
 };
+
 int main(int argc, char* argv[]){
-	//Выводим лого
-	logo();
-//Прверяем нужна ли пользователю помощь
- 	if ((!Cmp(argv[1],"--help")) || (!Cmp(argv[1],"--h"))){
-	help();
-		return 0;
- 	}
-//Проверка на верное количество аргументов
-	if (argc != 4){
-        cout << "Please get support by use --help" << endl;
-        return 0;
+    //Выводим лого
+    logo();
+    //Проверка на помощь
+    if (!strcmp(argv[1],"-help")){
+    	help();
+    //перенаправим поток вывода в файл 
+	freopen("1.txt", "w", stdout);
+	Plane tu;
+	Meteozond me;
+	Helicopter he;
+	Flying_robot fr;
+	if (!strcmp(argv[1],"-all")){
+		tu.getInfo();
+		me.getInfo();
+		he.getInfo();
+		fr.getInfo();
 	}
-  return 0;
+	else if(!strcmp(argv[1],"-one")){
+		if ((!strcmp(argv[2],"-Plane")))
+	    	tu.getInfo();
+	      
+	    if(!strcmp(argv[2],"-Meteozond"))
+	    	me.getInfo();
+	    
+	    if(!strcmp(argv[2],"-Helicopter"))
+	    	he.getInfo();
+
+	    if(!strcmp(argv[2],"-Flying_robot"))
+	    	fr.getInfo();
+	}  
+ 	fclose(stdout);
+    return 0;
 }
